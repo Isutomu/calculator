f1488e5b762eb51a1cae352f9c8dfebbc1c916bb
Isutomu <isutomu@gmail.com>
2024-04-10 12:43:32 -0300

    Add math operations functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(x, y) {
+    return Math.round((x+y) *100)/100;
+}
+
+function subtract(x, y){
+    return Math.round((x-y) *100)/100;
+}
+
+function multiply(x, y){
+    return Math.round((x*y) *100)/100;
+}
+
+function divide(x, y){
+    return Math.round((x/y) *100)/100;
+}
